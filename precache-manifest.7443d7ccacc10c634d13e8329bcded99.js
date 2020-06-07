fe000efa9a48741f3c6e5fd13731a835c3c5ba4d
Jiahua Liao <ljh15088131916@gmail.com>
2020-06-06 23:58:39 -0400

    Updates

---
+++ precache-manifest.7443d7ccacc10c634d13e8329bcded99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "333fb5b5b67e6b55797259885e4f429f",
+    "url": "/TTP-Assignment8/index.html"
+  },
+  {
+    "revision": "8713bcff9d101c19eaeb",
+    "url": "/TTP-Assignment8/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "afeb946d3f6d559f0f57",
+    "url": "/TTP-Assignment8/static/js/2.b2763f63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TTP-Assignment8/static/js/2.b2763f63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8713bcff9d101c19eaeb",
+    "url": "/TTP-Assignment8/static/js/main.a8c936fb.chunk.js"
+  },
+  {
+    "revision": "685e7a80796e427382cc",
+    "url": "/TTP-Assignment8/static/js/runtime-main.e03f3218.js"
+  }
+]);
